a5a1bd6edf9326a8a416cf85b7c7479e418838c2
Daniil Hayrapetyan <daniil.hayrapetyan@picsart.com>
2019-04-30 11:41:57 +0400

    main experiment

---
+++ experiment.fish
@@ -0,0 +1,7 @@
+python3 train.py \
+--dataset_mode texture \
+--name expression-transfer \
+--preprocess resize \
+--load_size 256 \
+--data-class-a ~/Projects-2017-11/emotion-transfer/test/data/common-1/images/texture-pat-dlib-512/common_part3_neutral \
+--data-class-b ~/Projects-2017-11/emotion-transfer/test/data/common-1/images/texture-pat-dlib-512/common_part1_middle_smile \
