467d14d7ba8b786a4a182d7f1f7fb31f34472722
Daniil Hayrapetyan <daniil.hayrapetyan@picsart.com>
2019-05-02 11:02:27 +0400

    try catch block for model pt saving

--- models/base_model.py
+++ models/base_model.py
@@ -160,10 +160,13 @@ class BaseModel(ABC):
                 else:
                     torch.save(net.cpu().state_dict(), save_path)
 
-                traced_net = torch.jit.trace(net.cuda(), torch.randn(1, 3, 256, 256).cuda())
-                save_filename = '%s_net_%s.pt' % (epoch, name)
-                save_path = os.path.join(self.save_dir, save_filename)
-                torch.jit.save(traced_net, save_path)
+                try:
+                    traced_net = torch.jit.trace(net.cuda(), torch.randn(1, 3, 256, 256).cuda())
+                    save_filename = '%s_net_%s.pt' % (epoch, name)
+                    save_path = os.path.join(self.save_dir, save_filename)
+                    torch.jit.save(traced_net, save_path)
+                except Exception as e:
+                    print("error saving model {}: {}".format(save_filename, e))
 
 
     def __patch_instance_norm_state_dict(self, state_dict, module, keys, i=0):
