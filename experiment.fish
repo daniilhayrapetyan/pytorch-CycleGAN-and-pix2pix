80c1a24009e5fe378fd1bc12729278742b3d47b6
Daniil Hayrapetyan <daniil.hayrapetyan@picsart.com>
2019-05-02 19:14:00 +0400

    replaced default pathnames

--- experiment.fish
+++ experiment.fish
@@ -4,6 +4,6 @@ python3 train.py \
 --preprocess resize \
 --load_size 400 \
 --save_epoch_freq 1 \
---data-class-a ~/Projects-2017-11/emotion-transfer/test/data/common-1/images/texture-pat-dlib-512/common_part3_neutral \
---data-class-b ~/Projects-2017-11/emotion-transfer/test/data/common-1/images/texture-pat-dlib-512/common_part1_middle_smile \
+--data-class-a ../data/common-1/images/texture-pat-dlib-512/common_part3_neutral \
+--data-class-b ../data/common-1/images/texture-pat-dlib-512/common_part1_middle_smile \
 --continue_train \
