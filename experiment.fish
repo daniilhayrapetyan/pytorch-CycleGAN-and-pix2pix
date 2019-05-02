0065a54b9ee51978a9b30b2b5c4794ee26438539
Daniil Hayrapetyan <daniil.hayrapetyan@picsart.com>
2019-05-02 18:36:50 +0400

    setup experiment name for base-x400

--- experiment.fish
+++ experiment.fish
@@ -1,8 +1,8 @@
 python3 train.py \
 --dataset_mode texture \
---name expression-transfer \
+--name expression-transfer-x400 \
 --preprocess resize \
---load_size 256 \
+--load_size 400 \
 --save_epoch_freq 1 \
 --data-class-a ~/Projects-2017-11/emotion-transfer/test/data/common-1/images/texture-pat-dlib-512/common_part3_neutral \
 --data-class-b ~/Projects-2017-11/emotion-transfer/test/data/common-1/images/texture-pat-dlib-512/common_part1_middle_smile \
